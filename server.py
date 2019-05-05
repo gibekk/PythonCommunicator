262f8dfdaf7ec571c490de951de77f995a77ae4a
gibekk <kamilgibowski90@gmail.com>
2019-05-05 15:22:37 +0200

    Finished server implementation

--- server.py
+++ server.py
@@ -1,4 +1,25 @@
 import socket
-localSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server = ('127.0.0.1', 8888)
-print "starting up on %s port %s" % server
+import sys
+serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server = ('127.0.0.1', 443)
+print >>sys.stderr, "starting up on %s port %s" % server
+serverSocket.bind(server)
+serverSocket.listen(1)
+
+while True:
+    print >>sys.stderr, "waiting for a connection"
+    connection, client_adress = serverSocket.accept()
+    try:
+        print >>sys.stderr, 'connection from', client_adress
+        while True:
+            data = connection.recv(1024)
+            print >>sys.stderr, "received '%s'" % data
+            if data:
+                print >>sys.stderr, 'sending data back to the client'
+                connection.sendall(data)
+            else:
+                print >>sys.stderr, 'no more data from', client_adress
+                break
+
+    finally:
+        connection.close()
